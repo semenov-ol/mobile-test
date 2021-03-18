1d0c8501106ce25da1644171d6b49dfe66cd6d41
Oleg Semenov <semenov.os16@gmail.com>
2021-03-18 17:27:55 +0200

    feat: Add apollo-client, graphql

--- src/screens/TabOneScreen.tsx
+++ src/screens/TabOneScreen.tsx
@@ -1,15 +1,36 @@
 import * as React from 'react';
 import { StyleSheet } from 'react-native';
+import { useQuery, gql } from '@apollo/client';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
 export default function TabOneScreen() {
+  const EXCHANGE_RATES = gql`
+    query GetExchangeRates {
+      rates(currency: "USD") {
+        currency
+        rate
+      }
+    }
+  `;
+
+  const { loading, data, error } = useQuery(EXCHANGE_RATES);
+
+  if (loading) return <Text>Loading...</Text>;
+  if (error) return <Text>Error :(</Text>;
+
+  function randomInteger(min: number, max: number) {
+    let rand = min + Math.random() * (max + 1 - min);
+    return Math.floor(rand);
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <Text>Random currency: {data.rates[randomInteger(1, 100)].currency}</Text>
     </View>
   );
 }
