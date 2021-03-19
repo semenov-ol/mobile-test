1f434d2e8df2297a5cd2c55b5d71ffd109576656
Oleg Semenov <semenov.os16@gmail.com>
2021-03-19 19:19:01 +0200

    feat: Add modal with data

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -5,8 +5,8 @@ import { ColorSchemeName } from 'react-native';
 
 import NotFoundScreen from '../screens/NotFoundScreen';
 import { RootStackParamList } from '../../types';
-import BottomTabNavigator from './BottomTabNavigator';
 import LinkingConfiguration from './LinkingConfiguration';
+import ModalNavigator from './ModalNavigator'
 
 // If you are not familiar with React Navigation, we recommend going through the
 // "Fundamentals" guide: https://reactnavigation.org/docs/getting-started
@@ -27,7 +27,7 @@ const Stack = createStackNavigator<RootStackParamList>();
 function RootNavigator() {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Root" component={BottomTabNavigator} />
+      <Stack.Screen name="Root" component={ModalNavigator} />
       <Stack.Screen name="NotFound" component={NotFoundScreen} options={{ title: 'Oops!' }} />
     </Stack.Navigator>
   );
--- src/screens/TabOneScreen.tsx
+++ src/screens/TabOneScreen.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
 import { inject, observer } from 'mobx-react';
 import { useQuery, gql } from '@apollo/client';
 
 import { Text, View } from '../components/Themed';
 import { toJS } from 'mobx';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { FC } from 'react';
 
-interface Rockets {
+export interface Rockets {
   name: string;
   __typename: string;
   boosters: number;
@@ -16,9 +18,16 @@ interface Rockets {
   };
 }
 
-function TabOneScreen(props: { store: { data: { rockets: Rockets[] }; updateData: (data: any) => {} } }) {
-  const { data: storeData, updateData } = props.store;
-  const [response, setResponse] = React.useState<{ rockets: Rockets[] }>();
+interface TabOneScreenProps {
+  navigation: StackNavigationProp<any>;
+  store: {
+    data: { rockets: Rockets[] };
+    updateData: (data: Rockets[]) => {};
+  };
+}
+
+const TabOneScreen: FC<TabOneScreenProps> = ({ store, navigation }) => {
+  const { data: storeData, updateData } = store;
 
   const EXCHANGE_RATES = gql`
     query GetRockets {
@@ -37,32 +46,29 @@ function TabOneScreen(props: { store: { data: { rockets: Rockets[] }; updateData
 
   React.useEffect(() => {
     updateData(data);
-    setResponse(toJS(storeData));
   }, [data]);
 
   return (
-    <View style={styles.container}>
-      <View style={{ width: '90%' }}>
+    <ScrollView style={styles.container}>
+      <View style={{ width: '90%', alignSelf: 'center' }}>
         <Text style={{ marginVertical: 20, fontWeight: 'bold' }}>SpaceX Rockets:</Text>
-        {response?.rockets?.map((item) => (
-          <View key={item.name}>
+        {toJS(storeData)?.rockets?.map((item: Rockets) => (
+          <TouchableOpacity onPress={() => navigation.navigate('Modal', { data: item })} key={item.name}>
             <Text>Rocket Name: {item.name}</Text>
             <Text>mass: {item.mass.kg}</Text>
             <Text>desc: {item.description}</Text>
             <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-          </View>
+          </TouchableOpacity>
         ))}
       </View>
-    </View>
+    </ScrollView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'column',
   },
   title: {
     fontSize: 20,
